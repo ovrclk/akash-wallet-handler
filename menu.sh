938211eab8c5d103be664c59a262cdd7fca21653
88plug <19512127+88plug@users.noreply.github.com>
2021-10-20 10:30:02 -0700

    Update menu.sh
    Add ability to send AKT / Display QR Code when showing address

--- menu.sh
+++ menu.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # menu.sh - Akash wallet handler is an easy way to create addresses and manage your AKT.
 
-docker build -t akash .
 
 #Detect self-contained build
 if [ -d akash-wallet-handler ]; then
+echo "Detected self contained build"
 cd akash-wallet-handler
+docker build -t akash .
+else
+docker build -t akash .
 fi
 
 #Read existing variables
@@ -83,8 +86,9 @@ letter of the choice as a hot key, or the \n\
 number keys 1-9 to choose an option.\n\
 Choose the TASK" 25 50 7 \
 Setup "First time setup and wallet creation" \
-Show "Show wallet address" \
-Check "Check wallet balance" \
+Show "Show wallet address and QR code" \
+Check "Check balance" \
+Send "Send AKT" \
 Node "Run full-node" \
 Export "Export private keys" \
 Run "Run command against CLI" \
@@ -105,11 +109,11 @@ fi
 
 ### display main menu ###
 
-function sayhello(){
-	local n=${@-"anonymous person"}
+#function sayhello(){
+#	local n=${@-"anonymous person"}
 	#display it
-	dialog --title "Hello" --clear --msgbox "Hello ${n}, let us be friends!" 10 41
-}
+#	dialog --title "Hello" --clear --msgbox "Hello ${n}, let us be friends!" 10 41
+#}
 
 
 function pass() {
@@ -145,6 +149,7 @@ fi
 function run_command(){
         command=$(dialog --inputbox "Command to run - example : help" 0 0  --output-fd 1)
 export command
+
 }
 function run_another(){
 read -p "Run another command? Continue (y/n)? " choice
@@ -155,6 +160,17 @@ case "$choice" in
 esac
 }
 
+function send_akt() {
+        receiveraddress=$(dialog --inputbox "Where to send AKT (address)?" 0 0  --output-fd 1)
+        receiveramount=$(dialog --inputbox "How much AKT (amount)?" 0 0  --output-fd 1)
+        export receiveramount
+	export receiveraddress
+}
+#function input_receiver_amount() {
+#        receiveramount=$(dialog --inputbox "How much AKT (amount)?" 0 0  --output-fd 1)
+#        receiveramount=$((1000*$receiveramount))
+#        export receiveramount
+#}
 function input_password() {
 
 if [ -d "data" ]
@@ -183,6 +199,22 @@ fi
 
 }
 
+function show_address(){
+#. variables
+#        echo "Setup finished succesfully at $(date) you can now send funds to $AKASH_ACCOUNT_ADDRESS. Your account must have funds before you can deploy an instance." >$OUTPUT
+
+docker exec -it akash /bin/bash -c "echo $pass | akash keys list | grep address | cut -d ':' -f2 | cut -c 2-" ; echo "" ;
+dialog --clear
+qrencode -t ASCIIi $(docker exec -it akash /bin/bash -c "echo $pass | akash keys list | grep address | cut -d ':' -f2 | cut -c 2-")
+sleep 10
+#> qrcode.log
+#cat qrcode.log > $OUTPUT
+#    display_output 0 600 "Your AKT Wallet Address"
+#dialog --no-collapse --msgbox "$(cat qrcode.log)" 640 480
+#dialog --stdout --tailbox qrcode.log 0 0
+
+#sleep 10
+}
 
 function finish(){
 . variables
@@ -223,9 +255,11 @@ case $menuitem in
 	Setup) setup;;
 	Node) ./run-full-node.sh;;
 	Export) ./backup-private-keys.sh  ; echo "Showing keys for 10 seconds!" ; sleep 10;;
-        Show) pass ; echo "Showing address for 10 seconds!" ; docker exec -it akash /bin/bash -c "echo $pass | akash keys list | grep address | cut -d ':' -f2 | cut -c 2-"; sleep 10;;
-        Check) echo "Showing address for 10 seconds!" ; docker exec -it akash /bin/bash -c 'akash query bank balances --node $AKASH_NODE $AKASH_ACCOUNT_ADDRESS' ; sleep 10;;
+        Show) pass ; show_address ;;
+#        Show) pass ; echo "Showing address for 10 seconds!" ; docker exec -it akash /bin/bash -c "echo $pass | akash keys list | grep address | cut -d ':' -f2 | cut -c 2-" ; echo "" ; qrencode -t ASCIIi '$(docker exec -it akash /bin/bash -c "echo $pass | akash keys list | grep address | cut -d ':' -f2 | cut -c 2-")' ; sleep 10;;
+        Check) echo "Showing balance for 10 seconds!" ; docker exec -it akash /bin/bash -c 'akash query bank balances --node $AKASH_NODE $AKASH_ACCOUNT_ADDRESS' ; sleep 10;;
         Run) run_command ; docker exec -it akash /bin/bash -c "akash $command" ; sleep 5 ; run_another;;
+        Send) send_akt ; pass ; docker exec -it akash /bin/bash -c "echo $pass | akash tx bank send "'"$AKASH_ACCOUNT_ADDRESS"'" $receiveraddress ${receiveramount}uakt --fees 200uakt --chain-id akashnet-2 -b async -y";;
 	Exit) echo "Bye"; break;;
 
 esac
