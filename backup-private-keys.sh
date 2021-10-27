03d641728fe04f96d0abd2aa67026b4ecfc53822
88plug <19512127+88plug@users.noreply.github.com>
2021-10-27 14:36:48 -0700

    Update backup-private-keys.sh
    Add mnemonic to export keys - useful in Akashlytics

--- backup-private-keys.sh
+++ backup-private-keys.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "Your private key is :"
 docker exec -it akash /bin/bash -c "akash keys export default"
+echo "Your mnemonic recovery phrase is :"
+docker exec -it akash /bin/bash -c "akash keys mnemonic"
