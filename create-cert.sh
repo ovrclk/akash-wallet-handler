f8a0ada515ef424040d6a0ea76e5b388cf718586
88plug <19512127+88plug@users.noreply.github.com>
2022-07-12 10:47:47 -0700

    Fix client cert creation

--- create-cert.sh
+++ create-cert.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Setting up certificate"
-docker exec -it akash /bin/bash -c 'akash tx cert create client --chain-id $AKASH_CHAIN_ID --keyring-backend $AKASH_KEYRING_BACKEND --from $AKASH_KEY_NAME --node $AKASH_NODE --fees 5000uakt'
+. variables
+#Generate Cert
+docker exec -it akash /bin/bash -c 'akash tx cert generate client --chain-id $AKASH_CHAIN_ID --keyring-backend $AKASH_KEYRING_BACKEND --from $AKASH_KEY_NAME --node $AKASH_NODE --fees 5000uakt'
+#Publish Cert
+docker exec -it akash /bin/bash -c 'akash tx cert publish client --chain-id $AKASH_CHAIN_ID --keyring-backend $AKASH_KEYRING_BACKEND --from $AKASH_KEY_NAME --node $AKASH_NODE --fees 5000uakt'
