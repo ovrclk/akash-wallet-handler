e4968a7e66f13db037007a02380a3973af45dcb3
88plug <19512127+88plug@users.noreply.github.com>
2022-07-12 10:48:25 -0700

    Update create-cert.sh

--- create-cert.sh
+++ create-cert.sh
@@ -4,4 +4,4 @@ echo "Setting up certificate"
 #Generate Cert
 docker exec -it akash /bin/bash -c 'akash tx cert generate client --chain-id $AKASH_CHAIN_ID --keyring-backend $AKASH_KEYRING_BACKEND --from $AKASH_KEY_NAME --node $AKASH_NODE --fees 5000uakt'
 #Publish Cert
-docker exec -it akash /bin/bash -c 'akash tx cert publish client --chain-id $AKASH_CHAIN_ID --keyring-backend $AKASH_KEYRING_BACKEND --from $AKASH_KEY_NAME --node $AKASH_NODE --fees 5000uakt'
+docker exec -it akash /bin/bash -c 'akash tx cert publish client --chain-id $AKASH_CHAIN_ID --keyring-backend $AKASH_KEYRING_BACKEND --from $AKASH_KEY_NAME --node $AKASH_NODE --fees 5000uakt -y'
